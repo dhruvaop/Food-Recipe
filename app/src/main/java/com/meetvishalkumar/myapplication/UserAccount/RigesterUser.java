e4a3e16d097d5f548ed12e07c9cdf4c018e23eeb
Dhruva Bhattacharya <71749153+dhruvaop@users.noreply.github.com>
2024-05-16 01:40:09 +0530

    Android app for foodies

---
+++ app/src/main/java/com/meetvishalkumar/myapplication/UserAccount/RigesterUser.java
@@ -0,0 +1,18 @@
+package com.meetvishalkumar.myapplication.UserAccount;
+
+
+public class RigesterUser {
+    public String Fullname, Email;
+
+
+    public RigesterUser(String _fullName, String _email, String _username, String _password, String _date, String _gender, int _age, int _birthyear) {
+
+    }
+
+    public RigesterUser(String Fullname, String Email) {
+        this.Fullname = Fullname;
+        this.Email = Email;
+
+    }
+
+}
